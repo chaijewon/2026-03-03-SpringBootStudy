6e99296e6a61b2f4661a1cf37aa555ee1cff716b
chaijewon <vcandjava@nate.com>
2026-08-03 11:36:03 +0900

    1111

---
+++ SpringThymeLeafProject_1/src/main/java/com/sist/web/repository/BoardRepository.java
@@ -0,0 +1,36 @@
+package com.sist.web.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+import com.sist.web.entity.*;
+// 단점 : JOIN이 어렵다 / SubQuery를 지원하지 않는다 
+// MyBatis 8:2 JPA 
+@Repository
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Integer>{
+	public BoardEntity findByNo(int no); // 상세보기 
+	
+	// 직접 SQL문장을 생성 => findAll(Page)
+	@Query(value="SELECT * FROM board ORDER BY no DESC "
+		 +"OFFSET :start ROWS FETCH NEXT 10 ROWS ONLY",
+		 nativeQuery = true) // SQL을 JPQL로 변경없이 문장 그대로 수행
+	public List<BoardEntity> boardListData(@Param("start") Integer start);
+	
+	/*
+	 *     findBy
+	 *         -- 
+	 *          WHERE no=1
+	 *     ---- SELECT *
+	 *     findByNameStartsWith  A%
+	 *          WHERE name Like
+	 *     findByNameEndsWith     %A
+	 *     findByNameContains   %A%
+	 *     findAll
+	 *     
+	 */
+	// save(update,insert) , delete 
+}
