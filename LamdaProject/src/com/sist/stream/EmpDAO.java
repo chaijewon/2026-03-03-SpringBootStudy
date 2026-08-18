f105582d105bebbc89656305e87455af4ff204ae
chaijewon <vcandjava@nate.com>
2026-08-18 11:38:31 +0900

    1111

--- LamdaProject/src/com/sist/stream/EmpDAO.java
+++ LamdaProject/src/com/sist/stream/EmpDAO.java
@@ -35,6 +35,32 @@ public class EmpDAO {
    {
 	   List<EmpVO> list=
 			   new ArrayList<EmpVO>();
+	   try
+	   {
+		   getConnection();
+		   String sql="SELECT empno,ename,job,hiredate,sal "
+				     +"FROM emp";
+		   ps=conn.prepareStatement(sql);
+		   ResultSet rs=ps.executeQuery();
+		   while(rs.next())
+		   {
+			   EmpVO vo=new EmpVO();
+			   vo.setEmpno(rs.getInt(1));
+			   vo.setEname(rs.getString(2));
+			   vo.setJob(rs.getString(3));
+			   vo.setHiredate(rs.getDate(4));
+			   vo.setSal(rs.getInt(5));
+			   list.add(vo);
+		   }
+		   rs.close();
+	   }catch(Exception ex)
+	   {
+		   ex.printStackTrace();
+	   }
+	   finally
+	   {
+		   disConnection();
+	   }
 	   return list;
    }
 }
--- LamdaProject/src/com/sist/stream/MainClass.java
+++ LamdaProject/src/com/sist/stream/MainClass.java
@@ -1,10 +1,85 @@
 package com.sist.stream;
-
+import java.util.*;
+import java.util.stream.Collectors;
 public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        EmpDAO dao=new EmpDAO();
+        List<EmpVO> list=dao.empAllData();
+        
+        // 전체 목록 출력 : forEach 
+        list.stream()
+            .forEach(vo->System.out.println(
+               vo.getEmpno()+" "
+               +vo.getEname()+" "
+               +vo.getSal()+" "
+               +vo.getJob()+" "
+               +vo.getHiredate().toString()
+             ));
+        System.out.println("======= filter1 =======");
+        list.stream()
+            .filter(vo->vo.getSal()>=3000)
+            .forEach(vo->System.out.println(
+                    vo.getEmpno()+" "
+                     +vo.getEname()+" "
+                     +vo.getSal()+" "
+                     +vo.getJob()+" "
+                     +vo.getHiredate().toString()
+                     ));
+        System.out.println("======= filter2 =======");
+        list.stream()
+            .filter(vo->vo.getEmpno()%2==0)
+                       // if(vo.getEmpno()%2==0)
+                       // 권한 
+            .forEach(vo->System.out.println(
+                    vo.getEmpno()+" "
+                     +vo.getEname()+" "
+                     +vo.getSal()+" "
+                     +vo.getJob()+" "
+                     +vo.getHiredate().toString()
+                     ));
+        System.out.println("======== sort ========");
+        list.stream()
+            .sorted(Comparator.comparing(EmpVO::getSal).reversed())
+            // order by sal asc  => reversed() desc
+            .forEach(vo->System.out.println(
+                    vo.getEmpno()+" "
+                     +vo.getEname()+" "
+                     +vo.getSal()+" "
+                     +vo.getJob()+" "
+                     +vo.getHiredate().toString()
+                     ));
+        // 중복 제거 
+        System.out.println("========== 중복제거 (distinct())=====");
+        List<String> kList=List.of("java","spring","html",
+        		  "css","javascript","html","css","vuejs");
+        kList.stream()
+             .distinct()
+             .forEach(System.out::println);
+        
+        // 통계 
+        System.out.println("======= reduce =======");
+        // mapreduce => Hadoop : 데이터마이닝 
+        int total=list.stream()
+        		      .map(EmpVO::getSal)
+        		      .reduce(0, Integer::sum);
+        System.out.println(total);
+        double avg=list.stream()
+  		      .mapToInt(EmpVO::getSal)
+  		      .average()
+  		      .orElse(0);
+        System.out.println(avg);
+        /*
+         *    distinct : 중복 제거 
+         *    reduce : 총합 => sum
+         *    average : 평균 => avg
+         *    sorted : 정렬 => order by
+         *    filter : 조건 => where 
+         *    map : 새로운 데이터 생성 
+         *    forEach : 반복문 
+         *    toList() : 데이터를 List로 묶어주는 경우 
+         */
 	}
 
 }
