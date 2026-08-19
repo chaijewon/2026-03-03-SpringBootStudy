0b3b472e41e0a7d18634fae3e411563cd2f2e5d7
chaijewon <vcandjava@nate.com>
2026-08-19 14:40:08 +0900

    1111

---
+++ SpringSecurityProject_1/src/main/java/com/sist/web/security/CustomUserDetailService.java
@@ -0,0 +1,60 @@
+package com.sist.web.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@Service // MyBatis연동 
+/*
+ *   ID: <input type=text name="username">
+ *   PW: <input type=password name="password"> 
+ *   
+ *   로그인 화면 
+ *     | POST => username=admin / password=1234
+ *       /login => 
+ *   Spring Security 
+ *     |
+ *   Authentication : 인증 
+ *     | - Success : 성공 => / (home.html)
+ *                   LoginSuccessHandler
+ *     | - Fail    : /login?error 
+ *                   LoginFailHandler
+ *                   -------- 아이디나 비밀번호가 틀립니다
+ *     => formLogin => 로그인 처리후 인증 => 해당 접속자의 정보를 읽는다 
+ *                     => id, password , enable , roles 
+ *                     => Principal 
+ *        logout => session해제 
+ *        
+ *     => Controller 
+ *        @GetMapping("/chat")
+ *        public String chat(HttpSession session,Pricipal p)
+ *        {
+ *             UserVO vo=dao.infoData(p.username)
+ *             session.setAttribute("vo",vo)
+ *        }
+ *                     
+ */
+public class CustomUserDetailService 
+implements UserDetailsService
+{
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		if(username.equals("admin"))
+		{
+			return User.builder()
+				  .username("admin")
+				  .password("{noop}1234") //{noop} 암호화없이 처리
+				  .roles("ADMIN")
+				  .build();
+		}
+		return User.builder()
+			   .username("user")
+			   .password("{noop}1234")
+			   .roles("USER")
+			   .build();
+	}
+
+}
